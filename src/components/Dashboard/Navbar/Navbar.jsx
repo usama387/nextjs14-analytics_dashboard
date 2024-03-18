744b33ea0f731ab99e54fe9f59f0804108a7649f
usama387 <128306049+usama387@users.noreply.github.com>
2024-03-18 10:06:30 +0500

    Navbar design completed along with dynamic title on every route

--- src/components/Dashboard/Navbar/Navbar.jsx
+++ src/components/Dashboard/Navbar/Navbar.jsx
@@ -1,8 +1,32 @@
+"use client";
 import React from "react";
 import styles from "./Navbar.module.css";
+import { usePathname } from "next/navigation";
+import {
+  MdNotifications,
+  MdOutlineChat,
+  MdPublic,
+  MdSearch,
+} from "react-icons/md";
 
 const Navbar = () => {
-  return <div className={styles.container}>Navbar</div>;
+  const pathname = usePathname();
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>{pathname.split("/").pop()}</div>
+      <div className={styles.menu}>
+        <div className={styles.search}>
+          <MdSearch />
+          <input type="text" placeholder="search..." className={styles.input} />
+        </div>
+        <div className={styles.icons}>
+          <MdNotifications size={20} />
+          <MdOutlineChat size={20} />
+          <MdPublic size={20} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
