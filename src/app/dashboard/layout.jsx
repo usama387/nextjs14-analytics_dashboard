822074d9b319ce7ef97d2465ffc9408f305aa6d7
usama387 <128306049+usama387@users.noreply.github.com>
2024-03-19 14:40:08 +0500

    Added one more component Footer along with styling in dashboard layout to make it common all dashboard routes

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "@/components/Dashboard/Navbar/Navbar";
 import Sidebar from "@/components/Dashboard/Sidebar/Sidebar";
 import styles from "./dashboard.module.css";
+import Footer from "@/components/Dashboard/Footer/Footer";
 
 // #1 This a layout page for dashboard which has different routes and therefore side and navbar will be visible on every page in all routes of dashboard
 const DashboardLayout = ({ children }) => {
@@ -13,6 +14,7 @@ const DashboardLayout = ({ children }) => {
       <div className={styles.content}>
         <Navbar />
         {children}
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Dashboard/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Usama Razaaq</div>
+      <div className={styles.text}>All rights reserved</div>
+    </div>
+  );
+};
+
+export default Footer;
