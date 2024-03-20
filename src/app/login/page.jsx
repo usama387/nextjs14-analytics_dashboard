c1aeb7889d23cf54890b54d85b8dbcca6655dc93
usama387 <128306049+usama387@users.noreply.github.com>
2024-03-20 22:45:46 +0500

    Login Page frontend completed

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import styles from "./login.module.css"
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <h1>LOGIN</h1>
+        <input type="text" name="username" placeholder="username" />
+        <input type="password" name="password" placeholder="password" />
+        <button>Login</button>
+      </form>
+    </div>
   )
 }
 
