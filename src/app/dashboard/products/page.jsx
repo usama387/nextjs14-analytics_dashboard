dccb324303404fc5b75e001cebed72758acd9897
usama387 <128306049+usama387@users.noreply.github.com>
2024-03-20 09:46:09 +0500

    Products Page frontend completed with reused pagination and search component

--- src/app/dashboard/products/page.jsx
+++ src/app/dashboard/products/page.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import Search from "@/components/Dashboard/Search/Search";
+import Link from "next/link";
+import Image from "next/image";
+import Pagination from "@/components/Dashboard/Pagination/Pagination";
+import styles from "./products.module.css";
 
 const ProductsPage = () => {
   return (
-    <div>ProductsPage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        {/* Search Component */}
+        <Search placeholder="search for a product..." />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        {/* thead represents titles of the table */}
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Createdat</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src="/noproduct.jpg"
+                  alt="user-avatar"
+                  width={40}
+                  height={40}
+                  className={styles.productImage}
+                />
+                IPhone 15 pro
+              </div>
+            </td>
+            <td>Desc</td>
+            <td>40,0000 pkr</td>
+            <td>19-03-2024</td>
+            <td>56</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href={"/"}>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      {/* Pagination Component */}
+      <Pagination />
+    </div>
+  );
+};
 
 export default ProductsPage;
