feaeacf25fc09f58da96a81653b10e38c8bb3961
usama387 <128306049+usama387@users.noreply.github.com>
2024-03-22 10:56:01 +0500

    Added some spaces in users page no code manipulated or changed

--- src/app/dashboard/users/page.jsx
+++ src/app/dashboard/users/page.jsx
@@ -7,12 +7,11 @@ import Pagination from "@/components/Dashboard/Pagination/Pagination";
 import { fetchUsers } from "@/lib/data";
 
 // Taking my modified query from search component to search users in the users page using searchParams to get the query
-
 const UsersPage = async ({ searchParams }) => {
   // #2 creating an instance of the query the ? means only when the query is available and then the query is passed into fetchUsers function
   const q = searchParams?.q || "";
 
-  // #3 instanceof the query for pagination show if particular page queri is available otherwise render first page and this instance of the query which is page is also passed into fetchUsers function.
+  // #3 instanceof the query for pagination show if particular page query is available otherwise render first page and this instance of the query which is page is also passed into fetchUsers function.
   const page = searchParams?.page || 1;
 
   // #1 fetchUsers is a function in data.js file to fetch user detail and total number of users from db
