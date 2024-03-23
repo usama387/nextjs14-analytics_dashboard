70e625cfdf57f29ad6a823c208c32ce751987b02
usama387 <128306049+usama387@users.noreply.github.com>
2024-03-23 12:32:52 +0500

    Added Clerk Auth and errorjsx file

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
 const HomePage = async () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <UserButton />
+      HomePage
+    </div>
+  );
+};
 
 export default HomePage;
